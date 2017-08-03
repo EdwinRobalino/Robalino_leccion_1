4c8089f37c10afe48140593c398d8097808c95b0
EdwinRobalino <edboroba@gmail.com>
2017-08-03 03:27:18 -0500

    vistas

---
+++ app/src/main/java/com/example/dptoredes/robalino_leccion_1/intento_2.java
@@ -0,0 +1,13 @@
+package com.example.dptoredes.robalino_leccion_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intento_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intento_2);
+    }
+}
